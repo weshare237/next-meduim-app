6d2c84a694be6aa8e65035fb5d82ef378e6e3779
weshare237 <fopaduclair2000@gmail.com>
2022-08-27 13:45:25 +0100

    Single post ssr rendering with isr

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,14 +15,14 @@ const Header: React.FC = () => {
         <div className='hidden md:inline-flex items-center space-x-5'>
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>
+          <h3 className='text-white bg-violet-500 px-4 py-1 rounded-full'>
             Follow
           </h3>
         </div>
       </div>
-      <div className='flex items-center space-x-5 text-green-600'>
+      <div className='flex items-center space-x-5 text-violet-600'>
         <h3>Sign In</h3>
-        <h3 className='border px-4 py-1 rounded-full border-green-600'>
+        <h3 className='border px-4 py-1 rounded-full border-violet-600'>
           Get Started
         </h3>
       </div>
